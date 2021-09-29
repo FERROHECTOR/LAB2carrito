11009425c4c8b356dc44562f225d60711ca24eb2
FERROHECTOR <hectorjferro@gmail.com>
2021-09-29 07:23:26 -0300

    Actualizacion

--- CarritoDeCompras/CarritoCompra.cs
+++ CarritoDeCompras/CarritoCompra.cs
@@ -6,18 +6,14 @@ using System.Threading.Tasks;
 
 namespace CarritoDeCompras
 {
+
+
     class CarritoCompra
     {
-        private List<Camisa> Camisas = new List<Camisa>();
-        private double DescuentoMedio 
-        {
-            get { return 10.0; }
-        }
 
-        private double DescuentoMayor
-        {
-            get { return 20.0; }
-        }
+        private List<Camisa> Camisas = new List<Camisa>();
+
+
 
         public void AgregarCamisa()
         {
@@ -33,46 +29,34 @@ namespace CarritoDeCompras
         }
         public int CantidadCamisas()
         {
-         var cantidad = Camisas.Count();
+            var cantidad = Camisas.Count();
             return cantidad;
         }
 
         public double PrecioCamisa()
         {
-            if(Camisas.Count()>0)
-            return Camisas.First().GetPrecio();
+
+            if (CantidadCamisas() > 0)
+                return Camisa.Precio;
             return 0.0;
-        }
-         
-        public double PrecioTotal()
-        {
-          double Total = 0.0;
-
-          foreach (Camisa c in Camisas)
-            {
-                Total += c.GetPrecio();
-            }
-            return Total;
-            
-
         }
 
         public double Descuento()
         {
-            if (Camisas.Count >= 3 && Camisas.Count <= 5)
-                return DescuentoMedio;
-            else if (Camisas.Count >= 5)
-                return DescuentoMayor;
-
-            return 0.0;
-
+            int cantidad = CantidadCamisas();
+            return Camisa.Descuento(cantidad);
         }
-
+        public double PrecioTotal()
+        {
+            int cantidad = CantidadCamisas();
+            return Camisa.PrecioTotal(cantidad);
+        }
         public double PrecioTotalConDescuento()
         {
-
-            return (PrecioTotal() * (100- Descuento()))/100;
+            int cantidad = CantidadCamisas();
+            return Camisa.PrecioTotalConDescuento(cantidad);
         }
+
 
     }
 
